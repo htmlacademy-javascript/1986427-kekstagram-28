1aace36b4d63ada46f3e725f03c4df5791a51b04
Евгений <evg-nikitin@mail.ru>
2023-02-28 23:52:20 +0300

    module2-task1

--- js/functions.js
+++ js/functions.js
@@ -23,11 +23,35 @@ const getDigits = (value) => {
   const digits = String(value).replace(/[^0-9]/g, '');
   return digits ? Number(digits) : NaN;
 };
+
 /**
  *
- * @param {string} value
+ * @param {string} val
  * @param {number} len
  * @param {string} pad
  * @returns {string}
  */
-const leftFill = (value, len, pad) => `${pad.repeat(len)}${value}`.slice(-len);
+const leftFill = (val, len, pad) => {
+  const str = String(val);
+  const currentPad = len - str.length;
+
+  if (currentPad <= 0) {
+    return str;
+  }
+  return pad.slice(0, currentPad % pad.length) + pad.repeat(currentPad / pad.length) + str;
+};
+
+// Добавочный символ использован один раз
+console.log(leftFill('1', 2, '0')); // '01'
+
+// Добавочный символ использован три раза
+console.log(leftFill('1', 4, '0')); // '0001'
+
+// Добавочные символы обрезаны с конца
+console.log(leftFill('q', 4, 'werty')); // 'werq'
+
+// Добавочные символы использованы полтора раза
+console.log(leftFill('q', 4, 'we')); // 'wweq'
+
+// Добавочные символы не использованы, исходная строка не изменена
+console.log(leftFill('qwerty', 4, '0')); // 'qwerty'
