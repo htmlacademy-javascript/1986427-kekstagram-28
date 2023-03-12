4166d5f2fd9071156da749ca7ed22a69a8a3de99
Евгений <evg-nikitin@mail.ru>
2023-03-12 21:58:02 +0300

    module5-task1

---
+++ js/utils.js
@@ -0,0 +1,18 @@
+export const getRandomInteger = (minNumber, maxNumber) => {
+  const lower = Math.ceil(Math.min(minNumber, maxNumber));
+  const upper = Math.floor(Math.max(minNumber, maxNumber));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+export const getArrayRandomElement = (items) => items[getRandomInteger(0, items.length - 1)];
+
+export const createIdGenerator = () => {
+  let lastId = 0;
+
+  return function () {
+    lastId += 1;
+    return lastId;
+  };
+};
