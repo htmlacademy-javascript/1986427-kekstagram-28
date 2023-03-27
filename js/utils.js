1ee667f3318a9eeeb5c4d049111afcd6a147f382
Евгений <evg-nikitin@mail.ru>
2023-03-27 23:45:30 +0300

    module9-task1

--- js/utils.js
+++ js/utils.js
@@ -17,4 +17,9 @@ export const createIdGenerator = () => {
   };
 };
 
+export const isArrayUnique = (tags) => {
+  const lowerTags = tags.map((tag) => tag.toLowerCase());
+  return lowerTags.length === new Set(lowerTags).size;
+};
+
 export const isEscapeKey = (evt) => evt.key === 'Escape';
