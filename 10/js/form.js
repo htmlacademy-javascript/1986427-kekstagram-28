49cb2c0168882b55093cc9c854885d8278b65b60
Keks <keks@htmlacademy.ru>
2023-04-02 23:30:49 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/form.js
@@ -0,0 +1,110 @@
+import {isArrayUnique, isEscapeKey} from './utils.js';
+import {Effect, setEffect, onEffectSliderUpdate, effectPicker, effectSlider} from './effects.js';
+import {Scale, onScaleControlClick, setScale, scaleControl, } from './scaler.js';
+import {UPLOAD_PHOTO, REQUEST_METHOD_POST, sendRequest} from './RequestApi.js';
+import {MESSAGE_TYPE_ERROR, MESSAGE_TYPE_SUCCESS, showMessage} from './MessageInfo.js';
+
+const form = document.querySelector('.img-upload__form');
+const uploadOverlay = document.querySelector('.img-upload__overlay');
+const uploadImageForm = document.querySelector('#upload-select-image');
+const hashtags = document.querySelector('.text__hashtags');
+const description = document.querySelector('.text__description');
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper'
+});
+
+const onImageLoadCloseClick = () => {
+  closeLoaderModal();
+};
+
+const onImageLoadEscKeyDown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeLoaderModal();
+  }
+};
+
+function closeLoaderModal () {
+  uploadImageForm.reset();
+
+  document.body.classList.remove('modal-open');
+  uploadOverlay.classList.add('hidden');
+
+  document.removeEventListener('keydown', onImageLoadEscKeyDown);
+}
+const sendForm = async () => {
+  const formData = new FormData(form);
+
+  form.querySelector('#upload-submit')
+    .setAttribute('disabled', '');
+
+  try {
+    await sendRequest(UPLOAD_PHOTO, {
+      method: REQUEST_METHOD_POST,
+      body: formData
+    });
+
+    form.querySelector('#picture-cancel').click();
+    showMessage(MESSAGE_TYPE_SUCCESS);
+  } catch {
+    showMessage(MESSAGE_TYPE_ERROR);
+  }
+
+  form.querySelector('#upload-submit').removeAttribute('disabled');
+};
+
+const onImageSubmit = async (evt) => {
+  evt.preventDefault();
+  if (pristine.validate()) {
+    await sendForm();
+    closeLoaderModal();
+  }
+};
+
+const validateHashtag = (hashtag) => new RegExp('^#[а-яёa-z0-9]{1,19}$').test(hashtag);
+
+const validateHashtags = (value) => {
+  const tags = value
+    .trim()
+    .split(' ')
+    .filter((tag) => tag.trim().length);
+  return tags.length <= 5 && isArrayUnique(tags) && tags.every(validateHashtag);
+};
+
+export const processingPhoto = () => {
+  setScale(Scale.MAX);
+  setEffect(Effect.NONE);
+
+  scaleControl.addEventListener('click', onScaleControlClick);
+  effectPicker.addEventListener('change', (event) => {
+    setEffect(event.target.getAttribute('value'));
+  });
+  effectSlider.on('update', onEffectSliderUpdate);
+};
+
+const onImageSelect = (event) => {
+  uploadOverlay.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  document.querySelector('#upload-cancel').addEventListener('click', onImageLoadCloseClick);
+  document.addEventListener('keydown', onImageLoadEscKeyDown);
+  if (event.target === form.filename) {
+    processingPhoto();
+  }
+};
+
+const onInputKeyDown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.stopPropagation();
+  }
+  pristine.addValidator(hashtags, validateHashtags, 'Неверный формат хэштэгов');
+};
+
+export const formListener = () => {
+  form.addEventListener('change', onImageSelect);
+  uploadImageForm.addEventListener('submit', onImageSubmit);
+  hashtags.addEventListener('keydown', onInputKeyDown);
+  description.addEventListener('keydown', onInputKeyDown);
+};
---
+++ 10/js/utils.js
@@ -0,0 +1,25 @@
+export const getRandomInteger = (minNumber, maxNumber) => {
+  const lower = Math.ceil(Math.min(minNumber, maxNumber));
+  const upper = Math.floor(Math.max(minNumber, maxNumber));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+export const getArrayRandomElement = (items) => items[getRandomInteger(0, items.length - 1)];
+
+export const createIdGenerator = () => {
+  let lastId = 0;
+
+  return function () {
+    lastId += 1;
+    return lastId;
+  };
+};
+
+export const isArrayUnique = (tags) => {
+  const lowerTags = tags.map((tag) => tag.toLowerCase());
+  return lowerTags.length === new Set(lowerTags).size;
+};
+
+export const isEscapeKey = (evt) => evt.key === 'Escape';
